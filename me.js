4ebd46c56d34782b56f8592edd6b63c321962817
HamishCooper <63433645+HamishCooper@users.noreply.github.com>
2025-09-04 14:03:27 +1200

    best update

---
+++ me.js
@@ -0,0 +1,34 @@
+// Panel-based ME data config with grid layout.
+window.meConfig = [{
+  name: "about",
+  title: "Who is me",
+  subtitle: "MULTI-MEDIA DESIGNER / DIRECTOR",
+  image: "images/me/tiaham.jpg",
+  team:  "Name; Hamish Cooper, Role; Project lead/design and sound | Name; Tia Cooper, Role; The best girl I know / Support | Name; Bonnie Mae Cooper, Role; Daughter + hilarious pest",
+  tools: "Photoshop, Illustrator, Cinema 4D, Unity 3D, Audacity, After Effects, Garage band",
+  title: "Who is me",
+  subtitle: "Hamish Cooper, an Englishman in New Zealand.",
+  meta: "Designer, creator, director, surfer, handyman...",
+  body: "One question guides my creative goal: Is it a nice place to be? If it is, people will stay. They’ll explore, play, spend time there — and maybe, if I’ve done my job right, they’ll tell their friends too.",
+  
+  layout: "grid",
+  blocks: [
+    { type: "text", span: 12, content: "Hi there. Thanks for taking the time to browse my work and please forgive me for talking about myself for this page. Design is at my core, an ongoing desire or need to create for anything and everything. Digitally, this is where I focus most of my energy. I design and produce a wide range of digital experiences: UI, UX, and CX, 3D modeling, animation, illustration, video and photo editing, visual effects, logos and logo animation, games, concepts, sound design, music and print. What matters most to me is crafting work that feels considered and purposeful, digital spaces and interactions that are inviting, engaging, and worth spending time with. When I’m not in front of a screen, I’m usually building, fixing, or planning to design something new, whether it’s crafting out of wood, painting, tinkering with an invention or even gardening."},
+    { type: "text", span: 12, content: "Since all my digital examples are on the main page I'll continue here with the more earthy version of my life."},
+    { type: "image", span: 12, src: "images/me/meathome.jpg" },
+    { type: "text", span: 8, content: "Yes, that is an old Apple monitor with some painting tools and wood tiles inside." },
+    { type: "image", src: "images/me/turfdesk.jpg" },
+    { type: "image", src: "images/me/family.jpg" },
+    { type: "image", src: "images/me/greenhousebuild.jpg" },
+    { type: "image", src: "images/me/greenhouse.jpg" },
+    { type: "image", src: "images/me/bw.jpg" },
+    { type: "image", src: "images/me/bowl.jpg" },
+    { type: "image", src: "images/me/skateboard.jpg" },
+    { type: "image", src: "images/me/pebbles.jpg" },
+    { type: "image", span: 12, src: "images/me/three.jpg" },
+    { type: "image", src: "images/me/cabinet.jpg" },
+    { type: "image", src: "images/me/surfing.jpg" },
+    { type: "text", content: "My classic turntable on top a cabinate I built." },
+  ]
+}];
+
